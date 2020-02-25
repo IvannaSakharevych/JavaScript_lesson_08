aa824964f0e4d280887d722b422d63f6d5a84589
Ivanna Sakharevych <Ivanna Sakharevych@192.168.1.12>
2020-02-25 16:53:24 +0200

    Events

---
+++ events.js
@@ -0,0 +1,18 @@
+"use strict"
+
+// task 1 - min
+// 1
+function Task1_1() {
+	text.classList.add('visibility');
+}
+
+// 2 
+function Task1_2() {
+	hideButton.classList.add('visibility');
+}
+
+
+// task 2 - max
+function Task2() {
+	Visibility = document.getElementById('list').classList.toggle('visibility');
+}
